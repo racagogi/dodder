7183b2733aea7b99ff51ccd4a90aad39e4958b48
racagogi <racagogi@gmail.com>
2023-05-01 21:29:47 +0900

    new

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,7 @@
-use clap::{Arg, ArgAction, Command};
+use dodder::cli;
 
 fn main() {
-    let matches = Command::new("dodder")
-        .about("file memo tree")
-        .version("0.0.1")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("add")
-                .short_flag('a')
-                .long_flag("add")
-                .about("add file to tree")
-                .arg(
-                    Arg::new("file")
-                        .short('f')
-                        .long("file")
-                        .help("added file")
-                        .action(ArgAction::Set),
-                )
-                .arg(
-                    Arg::new("position")
-                        .long("position")
-                        .short('p')
-                        .help("position to add node")
-                        .action(ArgAction::Set),
-                )
-                .arg(
-                    Arg::new("sibber")
-                        .short('b')
-                        .help("set position as sibber")
-                        .num_args(0),
-                )
-                .arg(
-                    Arg::new("last")
-                        .short('l')
-                        .help("insert last position")
-                        .num_args(0),
-                )
-                .arg(
-                    Arg::new("status")
-                        .short('s')
-                        .help("define file state")
-                        .action(ArgAction::Set),
-                )
-                .arg(
-                    Arg::new("name")
-                        .short('n')
-                        .help("set node name")
-                        .action(ArgAction::Set)
-                        .num_args(1..),
-                )
-                .arg(
-                    Arg::new("time")
-                        .short('t')
-                        .help("set status time")
-                        .action(ArgAction::Set),
-                ),
-        )
-        .subcommand(
-            Command::new("link")
-                .short_flag('l')
-                .about("link to nodes")
-                .arg(Arg::new("nodes").num_args(2).required(true)),
-        )
-        .subcommand(
-            Command::new("remove")
-                .arg(
-                    Arg::new("node")
-                        .short('n')
-                        .conflicts_with("link")
-                        .action(ArgAction::Set),
-                )
-                .arg(
-                    Arg::new("link")
-                        .short('l')
-                        .conflicts_with("node")
-                        .action(ArgAction::Set)
-                        .num_args(2),
-                ),
-        )
-        .subcommand(
-            Command::new("set")
-                .short_flag('s')
-                .arg(Arg::new("time").short('t'))
-                .arg(Arg::new("name").short('n'))
-                .arg(Arg::new("status").short('s')),
-        )
-        .get_matches();
+    let matches = cli::cli();
 
     match matches.subcommand() {
         Some(("add", add_matches)) => {
@@ -109,6 +24,10 @@ fn main() {
             println!("time {:?}", set_mathces.get_one::<String>("time"));
             println!("name {:?}", set_mathces.get_one::<String>("naem"));
             println!("status {:?}", set_mathces.get_one::<String>("status"));
+        }
+        Some(("move", move_mathces)) => {
+            println!("time {:?}", move_mathces.get_one::<String>("time"));
+            println!("name {:?}", move_mathces.get_one::<String>("naem"));
         }
         _ => unreachable!(),
     }
